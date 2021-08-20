6978a3feaf18322a3ee4575f2241f205df8a5e63
sanyamj138 <sanyamj138@gmail.com>
2021-08-20 17:09:26 +0530

    Version 2021.01

--- app/src/main/java/com/sanyamjain/tictactoe/MainActivity.java
+++ app/src/main/java/com/sanyamjain/tictactoe/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 //    TextView textView;
 //    GridLayout gridLayout;
 
-    // 0: cross, 1: ring
+    // 0 is for cross, 1 is for ring
     int activePlay = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2 , 2, 2};
     int[][] positions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 4, 8}, {3, 4, 6}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {2, 4, 6}};
